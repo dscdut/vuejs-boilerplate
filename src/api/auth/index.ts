09d0dd4fa8fb5d7788a922e114aa390d5d42a261
Nhân Nguyễn <nguyen.vh.nhan@gmail.com>
2024-06-29 11:02:20 +0700

    fix: api endpoint for Django APPEND_SLASH

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -9,23 +9,23 @@ import {
 } from './auth.dto'
 
 export async function apiLogin(payload: LoginRequest) {
-    return await $post<LoginResponse>('/auth/login', payload).then((resp) => {
+    return await $post<LoginResponse>('/auth/login/', payload).then((resp) => {
         return resp.data
     })
 }
 
 export async function apiLogout() {
-    await $post('/auth/logout')
+    await $post('/auth/logout/')
 }
 
 export async function apiRegister(payload: RegisterRequest) {
-    await $post<RegisterResponse>('/auth/register', payload).then((resp) => {
+    await $post<RegisterResponse>('/auth/register/', payload).then((resp) => {
         return resp.data
     })
 }
 
 export async function apiRefreshToken(payload: RefreshTokenRequest) {
-    return await $post<RefreshTokenResponse>('/auth/refresh', payload).then((resp) => {
+    return await $post<RefreshTokenResponse>('/auth/refresh/', payload).then((resp) => {
         return resp.data
     })
 }
